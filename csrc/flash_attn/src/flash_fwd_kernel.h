896585229b187da204ea06c70357ce41d5c64822
SeongBeomLEE <2712qwer@gmail.com>
2024-07-18 17:11:13 +0900

    [fix] apply_softcap

--- csrc/flash_attn/src/flash_fwd_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kernel.h
@@ -25,12 +25,15 @@ using namespace cute;
 template <typename Engine, typename Layout>
 __forceinline__ __device__ void apply_softcap(Tensor<Engine, Layout> &tensor, const float softcap){
     static_assert(Layout::rank == 3, "Only support 3D Tensor");
-    static_assert(decltype(size<0>(tensor_))::value == 4, "First dimension must be 4");
+    static_assert(decltype(size<0>(tensor))::value == 4, "First dimension must be 4");
+    #pragma unroll
     for (int i=0; i < size<0>(tensor); ++i){  // MMA
-        for (int mi; mi < size<1>(tensor); ++mi){
-            for (int nj; nj < size<2>(tensor); ++nj){
-                float tmp_val = fast_tanh(tensor(i, mi, nj) / softcap) * softcap
-                tensor(i, mi, nj) = tmp_val
+        #pragma unroll
+        for (int mi=0; mi < size<1>(tensor); ++mi){
+            #pragma unroll
+            for (int nj=0; nj < size<2>(tensor); ++nj){
+                float tmp_val = fast_tanh(tensor(i, mi, nj) * softcap);
+                tensor(i, mi, nj) = tmp_val;
             }
         }
     }
