79d23c8b28d9e87c06fbe85d8d3f157db384abc1
SeongBeomLEE <2712qwer@gmail.com>
2024-07-18 15:32:51 +0900

    [fix] apply_softcap

--- csrc/flash_attn/src/flash_fwd_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kernel.h
@@ -26,7 +26,7 @@ template <typename Engine, typename Layout>
 __forceinline__ __device__ void apply_softcap(Tensor<Engine, Layout> &tensor, const float softcap){
     #pragma unroll
     for (int i = 0; i < size(tensor); ++i) {
-        tensor(i) = tanh(tensor(i) * softcap);
+        tensor(i) = tanh(tensor(i) * static_cast<typename Tensor<Engine, Layout>::value_type>(softcap));
     }
 }
 
